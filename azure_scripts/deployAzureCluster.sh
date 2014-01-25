bd4518f69e45b523d0ddf1ee848fa0aa791fabd7
mwiewiorka <marek.wiewiorka@gmail.com>
2014-01-25 22:03:31 +0100

    Updates to the deployment script

--- azure_scripts/deployAzureCluster.sh
+++ azure_scripts/deployAzureCluster.sh
@@ -6,10 +6,33 @@ NC='\033[0m'  # No Color
 green='\033[32m'
 blue='\033[34m'
 
+instanceList="instances.txt"
+
+
 echo -e "${blue}Creating instances in Azure...${NC}\n" 
 sh ./createAzureCluster.sh
 
-sleep 180
+#check if all instance are up and running (barrier)
+allReady=0
+
+while [ $allReady -eq 0 ]
+do
+  allReady=1
+  while read host
+  do 
+    instStatus=`azure vm show $host | grep -i instancestatus | cut -f6 -d" " | sed s/\"//\g`
+    if [ "$instStatus" != "ReadyRole" ]
+    then
+      allReady=0
+      echo -e "${red}Instance ${host} not started...${NC}\n" 
+    else
+     echo -e "${green}Instance ${host} started...${NC}\n"   
+    fi	
+  done < ${instanceList}
+  sleep 30
+done
+
+
 echo -e "${blue}Copying priv keys to instances...${NC}\n" 
 sh ./copyKeyToAzureCluster.sh
 
