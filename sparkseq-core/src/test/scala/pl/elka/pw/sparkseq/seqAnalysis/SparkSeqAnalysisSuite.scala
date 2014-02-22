34d977311b2a70381165959765bb50117fe371fc
Marek Wiewiorka <marek.wiewiorka@gmail.com>
2014-02-22 13:38:34 +0000

    Fix in testsuite

--- sparkseq-core/src/test/scala/pl/elka/pw/sparkseq/seqAnalysis/SparkSeqAnalysisSuite.scala
+++ sparkseq-core/src/test/scala/pl/elka/pw/sparkseq/seqAnalysis/SparkSeqAnalysisSuite.scala
@@ -13,18 +13,18 @@ import org.scalatest.FunSuite
       sparkSeqTest("Test region count"){
         val pathExonsList = "../sparkseq/sparkseq-core/src/test/resources/sample_1.bed"
         val genExonsMapB = sc.broadcast(SparkSeqConversions.BEDFileToHashMap(sc,pathExonsList ))
-        val seqAnalysis = new SparkSeqAnalysis(sc,"../sparkseq/sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
+        val seqAnalysis = new SparkSeqAnalysis(sc,"sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
         val cov = seqAnalysis.getCoverageRegion(genExonsMapB)
         assert(seqAnalysis.getCoverageRegion(genExonsMapB).first()._2 === 3)
       }
 
       sparkSeqTest("Test base count"){
-        val seqAnalysis = new SparkSeqAnalysis(sc,"../sparkseq/sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
+        val seqAnalysis = new SparkSeqAnalysis(sc,"sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
         assert(seqAnalysis.getCoverageBase.filter(r=>(r._1==25001000011240L) ).first()._2 === 3)
       }
 
       sparkSeqTest("Test base count with filtered region"){
-        val seqAnalysis = new SparkSeqAnalysis(sc,"../sparkseq/sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
+        val seqAnalysis = new SparkSeqAnalysis(sc,"sparkseq-core/src/test/resources/sample_1.bam",25,1,4)
         assert(seqAnalysis.getCoverageBaseRegion("chr1",11000,16000).filter(r=>(r._1==25001000011240L) ).first()._2 === 3)
 
       }
