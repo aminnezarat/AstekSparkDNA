e0986be545e0eecaef6065fd7e474572e35b3aea
Marek Wiewiorka <marek.wiewiorka@gmail.com>
2014-03-22 11:36:52 +0000

    Cleanup

--- project/project/Plugins.scala
+++
@@ -1,7 +0,0 @@
-import sbt._
-
-    object Plugins extends Build {
-      val reporterPlugin = ProjectRef(new URI("git://github.com/mmarich/sbt-simple-junit-xml-reporter-plugin.git"),
-        "sbt-simple-junit-xml-reporter-plugin")
-      lazy val plugins = Project("plugins", file(".")).dependsOn(reporterPlugin)
-    }
